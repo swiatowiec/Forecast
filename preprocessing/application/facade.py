ec3c26b72069c6c8712cef2bb61085103574d732
swiatowiec <katarzynaswiatowiec@gmail.com>
2022-04-26 08:54:45 +0200

    transform facade init

--- preprocessing/application/facade.py
+++ preprocessing/application/facade.py
@@ -3,7 +3,8 @@ from domain.dataset_factory import DatasetFactory
 from domain.service import PreprocessingOptions, PreprocessingService
 from infrastructure.metadata import MetadataRepository
 from pydantic import BaseModel
-from typing import List
+from typing import List, Dict
+from domain.service import Metadata
 
 class PreprocessingFitTransformArgs(BaseModel):
     input_dir_path: str
@@ -39,3 +40,31 @@ class PreprocessingFitTransformFacade:
         self._metadata_repository.save_metadata(
             metadata=metadata.to_dict(), run_name=args.run_name)
 
+class PreprocessingTransformFacade:
+    def __init__(self,
+                 metadata_repository: MetadataRepository,
+                 preprocessing_service: PreprocessingService,
+                 dataset_repository: DatasetRepository,
+                 ):
+        self._metadata_repository = metadata_repository
+        self._preprocessing_service = preprocessing_service
+        self._dataset_repository = dataset_repository
+
+    def transform(self,
+                  measurements: List[Dict],
+                  run_name: str):
+
+        measurements_series = DatasetFactory.create_from_dict(measurements)
+
+        metadata_do = Metadata.from_dict(self._metadata_repository.get_metadata(
+            run_name=run_name))
+
+        preprocessing_options = PreprocessingOptions(fulfillment_mode=metadata_do.filler_metadata['filler_type'], 
+                                                    columns_to_fulfill=list(metadata_do.filler_metadata[
+                                                                                        'filler_value'].keys()),
+                                                    )
+
+        self._preprocessing_service.preprocess(dataset=measurements_series,
+                        preprocessing_options=preprocessing_options,
+                        metadata_do=metadata_do,
+                        )                              
