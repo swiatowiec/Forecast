b77b9e181fa9973bcd1bafd28b43c3ee5ae0d167
swiatowiec <katarzynaswiatowiec@gmail.com>
2022-05-13 20:56:16 +0200

    dvc config

--- training/run_train_process.py
+++ training/run_train_process.py
@@ -7,27 +7,11 @@ from config import TrainingConfig
 if __name__ == '__main__':
     container = TrainContainer()
     container.wire(modules=[sys.modules[__name__]])
-#dev    
-    parser = argparse.ArgumentParser(description='My program description')
-    parser.add_argument('--train_dir_path', type=str, default='data/train')
-    parser.add_argument('--n_splits', type=int, default=3)
-    parser.add_argument('--look_back', type=int, default=5)
-    parser.add_argument('--run_name', type=str, default='lookback_test')
-    parser.add_argument('--artifacts_dir_path', type=str,
-                        default='data/artifacts')
-    args = parser.parse_args()
-    perform_training(train_dir_path=args.train_dir_path,
-                    n_splits=args.n_splits,
-                    look_back=args.look_back,
-                    run_name=args.run_name,
-                    artifacts_dir_path=args.artifacts_dir_path,
-                    )
 
-#prod
     config = TrainingConfig('config.yaml')
     perform_training(train_dir_path=config.train_dir_path,
+                    artifacts_dir_path=config.artifacts_dir_path,
                      n_splits=config.n_splits,
                      look_back=config.look_back,
                      run_name=config.run_name,
-                     artifacts_dir_path=config.artifacts_dir_path,
                      )
